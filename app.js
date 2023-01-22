6001aba8f9cf776eab261f8a05769d977cd07a40
tpatil6 <tpatil6@uncc.edu>
2023-01-22 17:02:40 -0500

    "Express Code Final"

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-console.log("Hello world");
+const express = require('express')
+const app = express()
+const port = 3000
 
-// this is a demo
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
